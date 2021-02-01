bfed031b1063d34f39dfb22f46155c22ff9e1eb7
fishintaiwan <fershad@gmail.com>
2021-02-01 17:23:04 +0800

    add fathom for only prod

---
+++ src/components/Fathom.svelte
@@ -0,0 +1,8 @@
+<script>
+  export let siteCode;
+</script>
+
+{#if process.env.NODE_ENV === "production"}<script
+    src="https://cdn.usefathom.com/script.js"
+    data-site={siteCode}
+    defer></script>{/if}
--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -2,8 +2,13 @@
   import Header from "../components/Header.svelte";
   import Subscribe from "../components/Subscribe.svelte";
   import Footer from "../components/Footer.svelte";
+  import Fathom from "../components/Fathom.svelte";
   export let segment;
 </script>
+
+<svelte:head>
+  <Fathom siteCode="GYXRKGAO" />
+</svelte:head>
 
 <Header {segment} />
 
