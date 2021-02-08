85719a209d8a928e22dfaf62e76cb5acb80822ce
fishintaiwan <fershad@gmail.com>
2021-02-08 18:56:25 +0800

    add highlightjs to export

--- src/client.js
+++ src/client.js
@@ -1,5 +1,6 @@
 import * as sapper from '@sapper/app';
 import 'highlight.js/styles/solarized-dark.css';
+import hljs from 'highlight.js';
 
 sapper.start({
 	target: document.querySelector('#sapper')
