015f6a252b60bc97693cc5628ff6b6d51f5d95ef
fishintaiwan <fershad@gmail.com>
2021-02-09 11:40:31 +0800

    style links

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -21,19 +21,29 @@
     box-decoration-break: clone;
     -webkit-box-decoration-break: clone;
     background-image: linear-gradient(
-        var(--color-secondary-alt),
-        var(--color-secondary-alt)
-      ),
-      linear-gradient(var(--color-secondary), var(--color-secondary)),
-      linear-gradient(var(--color-primary), var(--color-primary)),
-      linear-gradient(var(--color-primary-alt), var(--color-primary-alt));
+      var(--color-primary-alt),
+      var(--color-primary-alt)
+    );
     background-repeat: no-repeat;
-    background-size: 1.5ch 0.3em, 3ch 0.3em, min(5ch, 75%) 0.3em, 100% 0.3em;
+    background-size: 100% 0.3em;
     background-position: 0 90%;
     margin: 0 auto var(--size-600) auto;
     font-size: var(--size-major);
     font-family: var(--heading-font);
     font-weight: 900;
+  }
+
+  @supports (background-image: url(foo.jpg), url(bar.jpg)) {
+    .title {
+      background-image: linear-gradient(
+          var(--color-primary-alt),
+          var(--color-primary-alt)
+        ),
+        linear-gradient(var(--color-primary), var(--color-primary)),
+        linear-gradient(var(--color-secondary), var(--color-secondary)),
+        linear-gradient(var(--color-secondary-alt), var(--color-secondary-alt));
+      background-size: 1.5ch 0.3em, 3ch 0.3em, min(5ch, 75%) 0.3em, 100% 0.3em;
+    }
   }
 
   header {
