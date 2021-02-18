6c56d9ea222456bed3401668ae8df7fc67c61310
fishintaiwan <fershad@gmail.com>
2021-02-18 20:39:11 +0800

    update style

--- src/routes/ogImage.svelte
+++ src/routes/ogImage.svelte
@@ -17,6 +17,7 @@
     {/if}
   </p>
   <h1>{title}</h1>
+  <hr />
   <p class="twitter">@fershad</p>
 </section>
 
@@ -26,33 +27,46 @@
     width: 100%;
     margin: 0;
     display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    grid-template-rows: 1fr 60px;
+    /* grid-template-columns: repeat(3, 1fr); */
+    grid-template-rows: 1fr auto 60px;
   }
 
   h1 {
     grid-row: 1;
-    grid-column: 1/-2;
+    /* grid-column: 1/-1; */
+    display: block;
     justify-self: center;
-    align-self: center;
-    box-decoration-break: clone;
-    -webkit-box-decoration-break: clone;
-    background-image: linear-gradient(
-      var(--color-primary-alt),
-      var(--color-primary-alt)
-    );
-    background-repeat: no-repeat;
-    background-size: 100% 0.3em;
-    background-position: 0 90%;
-    margin: 0 auto var(--size-600) auto;
+    align-self: flex-end;
+    text-align: center;
     font-size: var(--size-major);
     font-family: var(--heading-font);
     font-weight: 900;
+    padding: 0 var(--size-300);
+  }
+
+  hr {
+    background-image: linear-gradient(
+        var(--color-primary-alt),
+        var(--color-primary-alt)
+      ),
+      linear-gradient(var(--color-primary), var(--color-primary)),
+      linear-gradient(var(--color-secondary), var(--color-secondary)),
+      linear-gradient(var(--color-secondary-alt), var(--color-secondary-alt));
+    background-repeat: no-repeat;
+    background-position: 0% 100%, 25% 100%, 50% 100%, 75% 100%;
+    background-size: 35px 100%;
+    height: var(--size-600);
+    border: none;
+    width: 200px;
+    margin-block: var(--size-700);
+    grid-row: 2/3;
+    display: block;
+    /* grid-column: 1/-1; */
   }
 
   .twitter {
-    grid-row: 2;
-    grid-column: 3;
+    grid-row: 3;
+    /* grid-column: 3; */
     font-size: var(--size-600);
     text-align: right;
   }
