ba5d131f39a5bfa462174daaafffd4a6e229ce2c
fishintaiwan <fershad@gmail.com>
2021-02-18 20:07:08 +0800

    🤷🏾‍♂️

--- functions/ogImage/ogImage.js
+++ functions/ogImage/ogImage.js
@@ -36,10 +36,10 @@ async function getScreenshot(url) {
 exports.handler = async (event, context) => {
   const qs = new URLSearchParams(event.queryStringParameters);
   console.log(
-    `${process.env.URL || `http://localhost:8888`}/ogImage?${qs.toString()}`
+    `${process.env.URL}/ogImage?${qs.toString()}`
   );
   const photoBuffer = await getScreenshot(
-    `${process.env.URL || `http://localhost:8888`}/ogImage?${qs.toString()}`,
+    `${process.env.URL}/ogImage?${qs.toString()}`,
     // Here we need to pass a boolean to say if we are on the server. Netlify has a bug where process.env.NETLIFY is undefiend in functions so I'm using one of the only vars I can find
     // !process.env.NETLIFY
     process.env.URL.includes('http://localhost')
