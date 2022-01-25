698bad32df8535e28928084c3d8c415b0b86b271
Fershad <itsfish@fershad.com>
2022-01-25 14:35:17 +0800

    updates

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,5 +1,5 @@
 const dev = process.env.NODE_ENV !== 'production';
-const baseURL = dev ? 'http://localhost:8080' : 'https://fershad.com';
+const baseURL = dev ? 'http://localhost:8080' : 'https://optimised.email';
 const title = 'Fershad Irani';
 
 const privacyURL = `${baseURL}/privacy/`;
